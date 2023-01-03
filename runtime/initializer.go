268e0fdb3c1721d9111fdfaf61e8a12745b562b4
Noorvir Aulakh <noorvir.aulakh@gmail.com>
2023-01-03 10:17:52 +0100

    Move DBInitializer and unweave provider to infra

--- runtime/initializer.go
+++ runtime/initializer.go
@@ -8,43 +8,12 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/unweave/unweave-v2/providers/lambdalabs"
-	"github.com/unweave/unweave-v2/providers/unweave"
 	"github.com/unweave/unweave-v2/types"
 )
 
 type Initializer interface {
 	FromUser(userID uuid.UUID, provider types.RuntimeProvider) (*Runtime, error)
 	FromSession(sessionID string, provider types.RuntimeProvider) (*Runtime, error)
-}
-
-// DBInitializer is a runtime initializer that uses the database to store the runtime config
-// for each user, session, project, etc.
-type DBInitializer struct{}
-
-func (i *DBInitializer) FromUser(userID uuid.UUID, provider types.RuntimeProvider) (*Runtime, error) {
-	switch provider {
-	case types.LambdaLabsProvider:
-		sess, err := lambdalabs.NewSessionProvider("")
-		if err != nil {
-			return nil, err
-		}
-		return &Runtime{sess}, nil
-
-	case types.UnweaveProvider:
-		sess, err := unweave.NewSessionProvider("")
-		if err != nil {
-			return nil, err
-		}
-		return &Runtime{sess}, nil
-
-	default:
-		panic("Unknown runtime provider")
-	}
-	return nil, nil
-}
-
-func (i *DBInitializer) FromSession(sessionID string, provider types.RuntimeProvider) (*Runtime, error) {
-	return nil, nil
 }
 
 // ConfigFileInitializer is only used in development or if you're self-hosting Unweave.
@@ -85,16 +54,9 @@ func (i *ConfigFileInitializer) FromUser(userID uuid.UUID, provider types.Runtim
 		return &Runtime{sess}, nil
 
 	case types.UnweaveProvider:
-		sess, err := unweave.NewSessionProvider("")
-		if err != nil {
-			return nil, err
-		}
-		return &Runtime{sess}, nil
-
-	default:
-		panic("Unknown runtime provider")
+		return nil, fmt.Errorf("unweave provider not supported in config file initializer")
 	}
-	return nil, nil
+	return nil, fmt.Errorf("unknown runtime provider %q", provider)
 }
 
 func (i *ConfigFileInitializer) FromSession(sessionID string, provider types.RuntimeProvider) (*Runtime, error) {
