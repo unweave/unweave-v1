028d4415bede8a003ed8cf1cdeaf9a77d3f89209
Noorvir Aulakh <noorvir.aulakh@gmail.com>
2023-05-21 16:07:09 +0200

    Rename files

--- volume/volume.go
+++ volume/volume.go
@@ -2,90 +2,35 @@ package volume
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/unweave/unweave/api/types"
 )
 
-type Service struct {
-	store     Store
-	namespace string
-	provider  Provider
+type Provider interface {
+	// Create creates a volume. The size is in GB.
+	Create(ctx context.Context, size int) (Volume, error)
+	// Delete deletes the volume.
+	Delete(ctx context.Context) error
+	// Get gets the volume.
+	Get(ctx context.Context, id string) (Volume, error)
+	// List lists all volumes the provider has.
+	List(ctx context.Context) ([]Volume, error)
+	// Name returns the name of the provider.
+	Name() types.Provider
+	// Resize resizes the volume to the given size in GB.
+	Resize(ctx context.Context, size int) error
 }
 
-func NewVolumeService(namespace string, provider Provider, store Store) *Service {
-	return &Service{
-		store:     store,
-		namespace: namespace,
-		provider:  provider,
-	}
-}
-
-func (s *Service) Create(ctx context.Context, size int) (types.Volume, error) {
-	vol, err := s.provider.Create(ctx, size)
-	if err != nil {
-		return types.Volume{}, err
-	}
-
-	v, err := s.store.Create(s.namespace, vol.ID(), s.provider.Name().String())
-	if err != nil {
-		return types.Volume{}, fmt.Errorf("failed to add volume to store: %w", err)
-	}
-
-	return v, nil
-}
-
-func (s *Service) Delete(ctx context.Context, id string) error {
-	if err := s.provider.Delete(ctx); err != nil {
-		return fmt.Errorf("failed to delete volume with provider: %w", err)
-	}
-
-	if err := s.store.Remove(s.namespace, id); err != nil {
-		return fmt.Errorf("failed to remove volume from store: %w", err)
-	}
-	return nil
-}
-
-func (s *Service) Get(ctx context.Context, id string) (types.Volume, error) {
-	vol, err := s.store.Get(s.namespace, id)
-	if err != nil {
-		return types.Volume{}, fmt.Errorf("failed to get volume from store: %w", err)
-	}
-	return vol, nil
-}
-
-func (s *Service) List(ctx context.Context) ([]types.Volume, error) {
-	vols, err := s.store.List(s.namespace)
-	if err != nil {
-		return nil, fmt.Errorf("failed to list volumes from store: %w", err)
-	}
-	return vols, nil
-}
-
-func (s *Service) Mount(ctx context.Context, id, path string) error {
-	vol, err := s.store.Get(s.namespace, id)
-	if err != nil {
-		return fmt.Errorf("failed to get volume from store: %w", err)
-	}
-
-	v, err := s.provider.Get(ctx, vol.ID)
-	if err := v.Mount(ctx, path); err != nil {
-		return fmt.Errorf("failed to mount volume: %w", err)
-	}
-
-	return nil
-}
-
-func (s *Service) UnMount(ctx context.Context, id, path string) error {
-	vol, err := s.store.Get(s.namespace, id)
-	if err != nil {
-		return fmt.Errorf("failed to get volume from store: %w", err)
-	}
-
-	v, err := s.provider.Get(ctx, vol.ID)
-	if err := v.Unmount(ctx, path); err != nil {
-		return fmt.Errorf("failed to unmount volume: %w", err)
-	}
-
-	return nil
+// Volume is an interface that must be implemented by a volume.
+// The implementation of the volume interface should have the ability/permissions
+// to mount the volume onto a container inside a node. For instance, this could
+// be an AWS EBS volume mounted onto a container inside an EC2 machine, a FUSE
+// volume on top of S3/GCS, etc.
+type Volume interface {
+	// ID returns the ID of the volume.
+	ID() string
+	// Mount mounts the volume to the given path.
+	Mount(ctx context.Context, path string) error
+	// Unmount unmounts the volume from the given path.
+	Unmount(ctx context.Context, path string) error
 }
