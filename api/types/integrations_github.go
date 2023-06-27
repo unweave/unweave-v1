9b74928a154224a7d6a56375d9cb9782de3c328c
Callum Dempsey Leach <callum.leach@hotmail.co.uk>
2023-06-27 14:34:14 +0100

    Remove conversions

--- api/types/integrations_github.go
+++ api/types/integrations_github.go
@@ -1,34 +1,11 @@
 package types
 
-import (
-	"github.com/google/go-github/v53/github"
-)
-
 // CloneURL represents the URL Git repositories should be used to clone to
 type CloneURL = string
 
 type Repository struct {
 	Name     string   `json:"name,omitempty"`
 	CloneURL CloneURL `json:"cloneURL,omitempty"`
-}
-
-func NewRepository(gh *github.Repository) Repository {
-	return Repository{
-		Name:     gh.GetName(),
-		CloneURL: gh.GetCloneURL(),
-	}
-}
-
-func NewRepositories(gh []*github.Repository) []Repository {
-	out := make([]Repository, 0, len(gh))
-
-	for _, repository := range gh {
-		if repository != nil {
-			out = append(out, NewRepository(repository))
-		}
-	}
-
-	return out
 }
 
 // GithubListRepositoriesResponse lists all repositories authenticated to Unweave,
